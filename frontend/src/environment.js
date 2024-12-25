f53cfeb84afec97ffda2877a225218c66ce80864
prince <princeprajapati3848@gmail.com>
2024-12-25 14:10:38 +0530

    remove

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD=true;
 
 let server=IS_PROD ? 
-    "https://reacttodobackend-n7ho.onrender.com" :
+    "https://reacttodobackend-n7ho.onrender.com/" :
     "http://localhost:4001"
 
     export default server;
